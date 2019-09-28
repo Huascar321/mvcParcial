f6060a20f8f10075ea2c56cd84027f99258f8df3
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 10:03:22 -0400

    Ultimo commit

---
+++ Suarez/Huascar/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Huascar.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Huascar .Models.Fairy> Fairies { get; set; }
+    }
+}
---
+++ Suarez/Huascar/Models/Fairy.cs
@@ -0,0 +1,34 @@
+﻿namespace Huascar.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Fairy
+    {
+        [Key]
+        public int FairyID { get; set; }
+
+        
+        [Display(Name = "Nombre Completo")]
+        [StringLength(24, MinimumLength = 2)]
+        [Required]
+        public string NickName { get; set; }
+        public List Wishes { get; set; }
+        public enum List
+        {
+            Hamburguesa,
+
+            Harmonica,
+
+            Hielo,
+        }
+        [DataType(DataType.EmailAddress, ErrorMessage = "Este correo no es correcto")]
+        public string Email { get; set; }
+
+        //[Display(Name = "Nombre completo")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [Display(Name = "Cumpleaños")]
+        public DateTime Bithdate { get; set; }
+
+    }
+}
